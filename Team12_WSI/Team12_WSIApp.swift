8f1551bb06a58c3509ff95a088016b3964d8f739
Ishan Magarde <magardeishan@gmail.com>
2026-05-16 11:29:56 +0530

    initial commit

---
+++ Team12_WSI/Team12_WSIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Team12_WSIApp.swift
+//  Team12_WSI
+//
+//  Created by SDC-USER on 16/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Team12_WSIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
